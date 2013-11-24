c104be18fd64c7dcf04cd14e52c540bf2d084025
Kelly Crabbé <kelly.crabbe@gmail.com>
2013-11-24 01:25:32 +0100

    Finally, classcolored borders, names, statusbars. Guild was added too and AFK, DND, DEAD

--- core.lua
+++ core.lua
@@ -19,9 +19,11 @@ local tooltips = {   -- hijacking the tooltips
   ItemRefShoppingTooltip3,
   WorldMapTooltip
 }
+
 local RAID_CLASS_COLORS = RAID_CLASS_COLORS
 local FACTION_BAR_COLORS = FACTION_BAR_COLORS
 local GameTooltipStatusBar = GameTooltipStatusBar
+SetCVar("breakUpLargeNumbers", 1)
 
 ---------------------------------------------
 -- Config
@@ -35,26 +37,31 @@ local backdrop = {
     edgeSize = 1,
     insets = { left = -1, right = -1, top = -1, bottom = -1}
   }
-local backdropColor = { .1,.1,.1,1 }
-local borderColor = { .6,.6,.6,1 }
-local scale = 1
+local backdropColor = { r = 0.1, g = 0.1, b = 0.1, a = 1}
+local borderColor = { r = .6, g = .6, b = .6, a = 1 }
+local scale = 1.2
 local position = { "BOTTOMRIGHT", UIParent, "BOTTOMRIGHT", -10, 250 }
 
 ---------------------------------------------
 -- Functions
 ---------------------------------------------
 -- Changing the textures
-for i = 1, #tooltips do
+for i = 1, #tooltips do -- turns out the default color for non-textures tooltips is rgba(0,0,1,1)
   tooltips[i]:SetBackdrop(backdrop)
+  tooltips[i].SetBackdrop = function() end -- use empty func as dummy
   tooltips[i]:SetScale(scale)
-  tooltips[i]:SetBackdropColor(backdropColor.r, backdropColor.g, backdropColor.b)
-  tooltips[i]:SetBackdropBorderColor(backdropColor.r, backdropColor.g, backdropColor.b, backdropColor.a)
+  -- tooltips[i].SetBackdropColor = function() end -- use empty func as dummy
+  tooltips[i]:SetBackdropColor( backdropColor.r, backdropColor.g, backdropColor.b, backdropColor.a )
+  -- tooltips[i].SetBackdropBorderColor = function() end -- use empty func as dummy
+  tooltips[i]:SetBackdropBorderColor(borderColor.r, borderColor.g, borderColor.b, borderColor.a)
 end
 
 --change some text sizes
-GameTooltipHeaderText:SetFont(font, 14, "THINOUTLINE")
-GameTooltipText:SetFont(font, 12, "THINOUTLINE")
-Tooltip_Small:SetFont(font, 11, "THINOUTLINE")
+GameTooltipHeaderText:SetFont(font, 15, nil)
+GameTooltipText:SetFont(font, 13, nil)
+Tooltip_Small:SetFont(font, 12, nil)
+Tooltip_Small:SetShadowColor(0,0,0,1)
+Tooltip_Small:SetShadowOffset(1, -1)
 
 -- Reposition the HP bar
 GameTooltipStatusBar:ClearAllPoints()
@@ -75,9 +82,56 @@ hooksecurefunc("GameTooltip_SetDefaultAnchor", function(tooltip, parent)
     tooltip:SetPoint(unpack(position))
 end)
 
+-- Since we're moving around shit on the tooltip, we need to call this one too
+GameTooltip:HookScript("OnTooltipCleared", function(self)
+  self.unit = nil
+  GameTooltip_ClearStatusBars(self)
+end)
+
+-- GameTooltip_OnLoad()
+GameTooltip:HookScript("OnLoad", function(self)
+  -- -- assigning default DOESNT WORK
+  -- self:SetBackdropColor(backdropColor.r, backdropColor.g, backdropColor.b)
+  -- self:SetBackdropBorderColor(borderColor.r, borderColor.g, borderColor.b)
+end)
+
+-- Hook to colorize our shit
+GameTooltip:HookScript("OnTooltipSetUnit", function(self,...)
+  local unit = (select(2, self:GetUnit())) or (GetMouseFocus() and GetMouseFocus():GetAttribute("unit")) or (UnitExists("mouseover") and "mouseover") or nil
+  self:SetBackdropColor(backdropColor.r, backdropColor.g, backdropColor.b)
+  self.unit = nil
+
+  if unit then
+    self.unit = unit
+
+    if UnitIsPlayer(unit) then
+      -- if unit is a playable character -- classcolors!
+      local color = RAID_CLASS_COLORS[select(2, UnitClass(unit))]
+      GameTooltipStatusBar:SetStatusBarColor(color.r,color.g,color.b)
+      GameTooltipTextLeft1:SetTextColor(color.r,color.g,color.b)
+      GameTooltip:SetBackdropBorderColor(color.r, color.g, color.b)
+
+      -- if units are AFK/DND/DEAD/GHOST
+      if UnitIsAFK(unit) then
+        self:AppendText(" |cff00cccc<AFK>|r")
+      elseif UnitIsDND(unit) then
+        self:AppendText(" |cffcc0000<DND>|r")
+      elseif UnitIsDeadOrGhost(unit) then
+        self:AppendText(' |cffcc0000<DEAD>|r')
+      end
+
+      -- Guild Info
+      local unitGuild = GetGuildInfo(unit)
+      local text = GameTooltipTextLeft2:GetText()
+      if unitGuild and text and text:find("^"..unitGuild) then
+        GameTooltipTextLeft2:SetText("<"..text..">")
+        GameTooltipTextLeft2:SetTextColor(255/255, 20/255, 200/255)
+      end
+    end
+  end
+end)
+
 -- Funcs needed later on
--- GameTooltip_OnLoad(self)
--- GameTooltip_OnTooltipSetUnit
 -- GameTooltip_ShowStatusBar
 -- GameTooltip_Hide
 -- GameTooltip_OnTooltipSetItem
